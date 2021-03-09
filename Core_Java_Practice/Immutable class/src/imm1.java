c883b7926e4df57bea12638299e8998cc3461c53
7Roshani <roshumane@gmail.com>
2021-03-09 09:20:56 +0530

    java

---
+++ Core_Java_Practice/Immutable class/src/imm1.java
@@ -0,0 +1,23 @@
+final class Employee
+{
+    final String pancardNumber;
+
+    public Employee(String pancardNumber){
+        this.pancardNumber=pancardNumber;
+    }
+
+    public String getPancardNumber(){
+        return pancardNumber;
+    }
+
+}
+
+
+public class imm1
+{
+    public static void main(String[] arg)
+    {
+        Employee e=new Employee("abc123");
+        System.out.println("pancard no " + e.getPancardNumber());
+    }
+}
---
+++ Core_Java_Practice/StringBuffer and StringBuilder/src/BufferTest.java
@@ -0,0 +1,8 @@
+public class BufferTest {
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer("hello");
+        buffer.append("java");
+        System.out.println(buffer);
+    }
+
+}
